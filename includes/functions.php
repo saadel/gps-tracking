414dd1cf71fd3dda06a2bfa3c304e8df92d10b6e
saadel <saadelha@gmail.com>
2014-08-12 23:34:13 +0100

    add functions.php

---
+++ includes/functions.php
@@ -0,0 +1,21 @@
+<?php 
+    function plotAll()
+    {        
+        $sql = "SELECT * FROM gpslocations";
+        $sql .= "WHERE phoneNumber=:name;";
+        return $sql;
+    }
+
+    function plotLast()
+    {
+        $sql = "SELECT * FROM gpslocations WHERE phoneNumber=:name";
+        $sql .= " ORDER BY LastUpdate desc";
+        $sql .= "LIMIT 1;";
+        return $sql;
+    }
+
+    // function plotEveryone($name)
+    // {
+    //     # code...
+    // }
+?>
